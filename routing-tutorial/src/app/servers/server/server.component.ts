7ada6a7554a918382dde81c9c984565310aa4716
Józek <przyjemniaczek@gmail.com>
2019-04-16 21:47:10 +0200

    routing exercise
    Done some exercises with router.navigate and setting up a subscribe method on query params.
    Sorry for lack of longer explanation but it's boring as fuck. Luckily code will provide answers you need.
    Just git show'it and you're there.

--- routing-tutorial/src/app/servers/server/server.component.ts
+++ routing-tutorial/src/app/servers/server/server.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -10,7 +10,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService,private route:ActivatedRoute) { }
+  constructor(private serversService: ServersService,private route:ActivatedRoute, private router:Router) { }
 
   ngOnInit() {
     this.server = this.serversService.getServer(+this.route.snapshot.params['id']);
@@ -20,6 +20,11 @@ export class ServerComponent implements OnInit {
         this.server = this.serversService.getServer(+params['id']);
       }
     );
+  }
+
+  onEdit(){
+    //this.router.navigate(['/servers',this.server.id,'edit']);
+    this.router.navigate(['edit'],{relativeTo:this.route});
   }
 
 }
